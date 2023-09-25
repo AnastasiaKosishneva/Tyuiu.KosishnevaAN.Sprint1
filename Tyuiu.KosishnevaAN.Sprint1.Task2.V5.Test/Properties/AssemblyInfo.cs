d3d941c089f4408a782b89d1f2f43b1f28ce5896
Tyuiu.KosishnevaAN@outlook.com <nastyakosishneva@gmail.com>
2023-09-25 21:03:22 +0500

    Task2

---
+++ Tyuiu.KosishnevaAN.Sprint1.Task2.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KosishnevaAN.Sprint1.Task2.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KosishnevaAN.Sprint1.Task2.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5d956aba-9294-48a9-adc6-df30cb41cf61")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.kosishnevaAN.Sprint1.Task2.V5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KosishnevaAN.Sprint1.Task2.V5.Lib;
+
+namespace Tyuiu.kosishnevaAN.Sprint1.Task2.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Косишнева А. Н. | ИСТНб-23-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #1                                                             *");
+            Console.WriteLine("* Тема: Базовые навыки в C#                                             *");
+            Console.WriteLine(" Задание #2                                                             *");
+            Console.WriteLine(" Вариант #5                                                             *");
+            Console.WriteLine(" Выполнила: Косишнева Анастасия Николаенвна | ИСТНб-23-1                *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                              *");
+            Console.WriteLine(" Известна длина стороны куба. Вычислить площадь боковой поверхности     *");
+            Console.WriteLine(" куба.                                                                  *");
+            Console.WriteLine("*                                                                       *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                      *");
+            Console.WriteLine("*************************************************************************");
+            int x;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("площадь боковой поверхности куба = " + ds.CalculateSideSquare(x));
+            Console.ReadKey();
+        }
+    }
+}
