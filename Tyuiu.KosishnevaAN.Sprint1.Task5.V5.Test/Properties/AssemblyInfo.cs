94794e1ce197498ea64b7843e996c8fa05d36aba
Tyuiu.KosishnevaAN@outlook.com <nastyakosishneva@gmail.com>
2023-09-26 23:43:04 +0500

    Task5

---
+++ Tyuiu.KosishnevaAN.Sprint1.Task5.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KosishnevaAN.Sprint1.Task5.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KosishnevaAN.Sprint1.Task5.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5dc06c7d-caeb-42b6-b750-0e1ce05bb87a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KosishnevaAN.Sprint1.Task5.V5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KosishnevaAN.Sprint1.Task5.V5.Lib;
+
+namespace Tyuiu.KosishnevaAN.Sprint1.Task5.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Косишнева А. Н. | ИСТНб-23-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #1                                                             *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                            *");
+            Console.WriteLine("* Задание #5                                                            *");
+            Console.WriteLine("* Вариант #5                                                            *");
+            Console.WriteLine("* Выполнила: Косишнева Анастасия Николаенвна | ИСТНб-23-1               *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                              *");
+            Console.WriteLine("* Присвоить целой переменной d первую цифру из дробной части            *");
+            Console.WriteLine("* положительного вещественного числа x (так, если x=32.597, то d=5).    *");
+            Console.WriteLine("*                                                                       *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                      *");
+            Console.WriteLine("*************************************************************************");
+            double x;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+            int res = Convert.ToInt32(ds.Calculate(x));
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
